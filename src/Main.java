5cffd399f116f418d0e213659bda6cff0fb07190
Gustavo Bauke <baukegustavo@gmail.com>
2024-09-08 12:32:33 -0300

    adds CLI

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,14 @@
-import io.compiler.core.generated.IsiLangLexer;
-import io.compiler.core.generated.IsiLangParser;
-import io.interpreter.Interpreter;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
+import io.cli.ArgumentParser;
+import io.cli.CLI;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            IsiLangLexer lexer;
-            IsiLangParser parser;
+            var argumentParser = new ArgumentParser();
+            var arguments = argumentParser.parse(args);
+            var cli = new CLI(arguments.target, arguments.input, arguments.output);
 
-            lexer = new IsiLangLexer(CharStreams.fromFileName("input.isi"));
-            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-            parser = new IsiLangParser(tokenStream);
-
-            parser.program();
-            var program = parser.getProgram();
-
-            System.out.println("Java Target:\n" + program.generateJavaTarget());
-            System.out.println("\n\n\n\n");
-            System.out.println("C Target:\n" + program.generateCTarget());
-
-            var interpreter = new Interpreter(program);
-            interpreter.run();
+            cli.run();
         } catch (Exception ex) {
             System.err.println("Error: " + ex.getMessage());
             System.out.println("Stack trace:");
---
+++ src/io/compiler/targets/java/JavaGenerator.java
@@ -0,0 +1,30 @@
+package io.compiler.targets.java;
+
+import io.compiler.core.program.Program;
+import io.compiler.targets.IGenerator;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class JavaGenerator implements IGenerator {
+    @Override
+    public String generate(Program program) {
+        return program.generateJavaTarget();
+    }
+
+    @Override
+    public void writeToFile(Program program, String path) {
+        var javaCode = generate(program);
+
+        try {
+            var writer = new BufferedWriter(new FileWriter(path));
+
+            writer.write(javaCode);
+            writer.close();
+        } catch (Exception exception) {
+            System.err.println("Error: " + exception.getMessage());
+            System.out.println("Stack trace:");
+            exception.printStackTrace();
+        }
+    }
+}
